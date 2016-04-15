22066593eb78ff40c64c1b061c5b165e4e842b49
Mark Lawrence <marklawrence72@gmail.com>
2016-04-15 12:55:07 -0500

    Add project files.

---
+++ crmc.data/Class1.cs
@@ -0,0 +1,16 @@
+﻿using crmc.domain;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace crmc.data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+            Database.Log = msg => Debug.WriteLine(msg);
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
