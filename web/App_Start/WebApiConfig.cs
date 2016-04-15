7e3bf746279ed899b6e84573d660488409ab0fbb
Mark Lawrence <marklawrence72@gmail.com>
2016-04-15 14:40:28 -0500

    person webapi

--- web/App_Start/WebApiConfig.cs
+++ web/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace web
@@ -10,6 +12,11 @@ namespace web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ web/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using crmc.data;
+using System.Linq;
+using System.Web.Http;
+
+namespace web.Controllers
+{
+    [RoutePrefix("api/person")]
+    public class PersonController : ApiController
+    {
+        private readonly DataContext context;
+
+        public PersonController()
+        {
+            context = new DataContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var list = context.Persons.OrderBy(x => x.SortOrder).Skip(10).Take(10);
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int take, int skip)
+        {
+            var list = context.Persons.OrderBy(x => x.SortOrder).Skip(skip).Take(take);
+            return Ok(list);
+        }
+
+        [HttpGet, Route("Count")]
+        public IHttpActionResult Count()
+        {
+            var count = context.Persons.Count();
+            return Ok(count);
+        }
+
+        [HttpGet, Route("CountDistinct")]
+        public IHttpActionResult CountDistinct()
+        {
+            var count = context.Persons.GroupBy(x => new { x.Lastname, x.Firstname }).Count();
+            return Ok(count);
+        }
+    }
+}
