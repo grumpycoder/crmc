353eca4c076781268d0261c3a66baf2ca1d75742
Mark Lawrence <marklawrence72@gmail.com>
2016-04-18 11:34:48 -0500

    added signalr references and hub to web

---
+++ web/Hubs/WotHub.cs
@@ -0,0 +1,16 @@
+﻿using crmc.domain;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+
+namespace web.Hubs
+{
+    [HubName("wot")]
+    public class WotHub : Hub
+    {
+        public Task AddName(string kiosk, Person person)
+        {
+            return Clients.All.addName(kiosk, person);
+        }
+    }
+}
---
+++ web/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(web.Startup))]
+
+namespace web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConfiguration = new HubConfiguration
+            {
+                EnableDetailedErrors = true,
+                EnableJSONP = true
+            };
+            app.MapSignalR(hubConfiguration);
+        }
+    }
+}
