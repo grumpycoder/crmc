82cbd3a0a4dd76b0c81fdcb343f3e0c35ad669f3
grumpycoder <marklawrence72@gmail.com>
2016-04-28 09:10:59 -0500

    settings sliders

--- web/app/configuration/manager.js
+++ web/app/configuration/manager.js
@@ -4,15 +4,25 @@
 (function () {
     var controllerId = 'SettingsController';
 
-    angular.module('app.settings').controller(controllerId, ['$log', 'configurationService', mainController]);
+    angular.module('app.settings').controller(controllerId, ['$log', '$timeout', 'configurationService', mainController]);
 
-    function mainController(log, service) {
+    function mainController(log, $timeout, service) {
         var vm = this;
 
         vm.config = {};
         vm.save = save;
 
         activate();
+
+        vm.minRangeSlider = {
+            minValue: 10,
+            maxValue: 90,
+            options: {
+                floor: 0,
+                ceil: 100,
+                step: 1
+            }
+        };
 
         function activate() {
             log.info('settings controller active');
